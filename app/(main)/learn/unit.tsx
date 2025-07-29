9610809b23a49ef05e4801ac735c7d4197aede1a
Felipe de Oliveira Sousa <felipe.oliveira@seasolutions.io>
2025-07-29 11:32:18 -0300

    Hotfix: build
    - Correções de tipagem para build
    - Remoção de regras lint para interromper warnings
    - Adição de pasta node_modules no arquivo gitignore
    -

--- app/(main)/learn/unit.tsx
+++ app/(main)/learn/unit.tsx
@@ -3,38 +3,54 @@ import { UnitBanner } from "./unit-banner";
 import { LessonButton } from "./lesson-button";
 
 type Props = {
-    id:number;
-    order:number;
-    title:string; 
-    description:string;
-    lessons:(typeof lessons.$inferSelect&{
-        completed:boolean;
-    })[];
+  id: number;
+  order: number;
+  title: string;
+  description: string;
+  lessons: (typeof lessons.$inferSelect & {
+    completed: boolean;
+    id: number | undefined;
+  })[];
 
-    activeLesson:typeof lessons.$inferSelect & {
-        unit: typeof units.$inferSelect
-    } | undefined;
+  activeLesson:
+    | (typeof lessons.$inferSelect & {
+        unit: typeof units.$inferSelect;
+      })
+    | undefined;
 
-    activeLessonPercentage:number;
-}
+  activeLessonPercentage: number;
+};
 
-export const Unit = ({id, order, title,description, lessons,activeLesson, activeLessonPercentage}: Props) =>{
-    return(
-        <>
-          <UnitBanner title={title} description={description}/>
-          <div className="flex items-center flex-col relative">
-            {lessons.map((lesson,index)=>{
-            const isCurrent = lesson.id === activeLesson?.id;
-            const isLocked = !lesson.completed && !isCurrent;
+export const Unit = ({
+  id,
+  order,
+  title,
+  description,
+  lessons,
+  activeLesson,
+  activeLessonPercentage,
+}: Props) => {
+  return (
+    <>
+      <UnitBanner title={title} description={description} />
+      <div className="flex items-center flex-col relative">
+        {lessons.map((lesson, index) => {
+          const isCurrent = lesson.id === activeLesson?.id;
+          const isLocked = !lesson.completed && !isCurrent;
 
-            return(
-                <LessonButton 
-                key={lesson.id} id={lesson.id} index={index}
-                totalCount={lessons.length-1} current={isCurrent}
-                locked={isLocked} percentage={activeLessonPercentage}/>
-            );
-          })}
-          </div>
-        </>
-    )
-}
+          return (
+            <LessonButton
+              key={lesson.id}
+              id={lesson.id}
+              index={index}
+              totalCount={lessons.length - 1}
+              current={isCurrent}
+              locked={isLocked}
+              percentage={activeLessonPercentage}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
--- app/lesson/[lessonId]/page.tsx
+++ app/lesson/[lessonId]/page.tsx
@@ -2,34 +2,36 @@ import { getLesson, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Quiz } from "../quiz";
 
-type Props = { 
-    params: {
-        lessonId:number;
-    };
-};
+export default async function LessonIdPage({
+  params,
+}: {
+  params: Promise<{ lessonId: string }>;
+}) {
+  const { lessonId } = await params;
+  const lessonData = getLesson(Number(lessonId));
+  const userProgressData = getUserProgress();
 
+  const [lesson, userProgress] = await Promise.all([
+    lessonData,
+    userProgressData,
+  ]);
 
-const LessonIdPage =async ({params}:Props) =>{
+  if (!lesson || !userProgress) {
+    redirect("/learn");
+  }
 
-    const lessonData = getLesson(params.lessonId);
-    const userProgressData = getUserProgress();
+  const initialPercentage =
+    (lesson.challenges.filter((challenge) => challenge.completed).length /
+      lesson.challenges.length) *
+    100;
 
-    const [lesson, userProgress] = await Promise.all([
-        lessonData,
-        userProgressData
-    ])
-
-    if(!lesson || !userProgress) {
-        redirect("/learn");
-    }
-
-    const initialPercentage = lesson.challenges.filter((challenge) => challenge.completed).length / lesson.challenges.length *100;
-
-    return(
-        <Quiz initialLessonId={lesson.id} initialLessonChallenges={ lesson.challenges} 
-        initialHearts={userProgress.vidas} initialPercentage={initialPercentage} userSubscripstion={null}/>
-            
-    );
-};
-
-export default LessonIdPage;
+  return (
+    <Quiz
+      initialLessonId={lesson.id}
+      initialLessonChallenges={lesson.challenges}
+      initialHearts={userProgress.vidas}
+      initialPercentage={initialPercentage}
+      userSubscripstion={null}
+    />
+  );
+}
